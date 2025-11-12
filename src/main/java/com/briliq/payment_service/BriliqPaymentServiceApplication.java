8352a82338e4f2618a0e96fc5560c5a690d1d6ba
Dandi <d.agusmaulana99@gmail.com>
2025-11-12 10:55:08 +0700

    Initial Spring Boot Payment Service

---
+++ src/main/java/com/briliq/payment_service/BriliqPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.briliq.payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriliqPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriliqPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briliq/payment_service/BriliqPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briliq.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriliqPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
