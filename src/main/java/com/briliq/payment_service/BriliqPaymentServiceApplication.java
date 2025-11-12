531dcda38e03716d94336aeb22e1f7b4d67f3bb1
Dandi <d.agusmaulana99@gmail.com>
2025-11-12 11:52:38 +0700

    test controller

--- src/main/java/com/briliq/payment_service/BriliqPaymentServiceApplication.java
+++ src/main/java/com/briliq/payment_service/BriliqPaymentServiceApplication.java
@@ -2,12 +2,12 @@ package com.briliq.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BriliqPaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BriliqPaymentServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BriliqPaymentServiceApplication.class, args);
+    }
 }
